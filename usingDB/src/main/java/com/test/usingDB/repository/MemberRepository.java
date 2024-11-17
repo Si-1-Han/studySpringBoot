d92c04ba1918642d2ea5b1317bd314f90e41858c
Si-1-Han <hswnemo@gmail.com>
2024-11-17 18:10:23 +0900

    11/15 commit for login

--- usingDB/src/main/java/com/test/usingDB/repository/MemberRepository.java
+++ usingDB/src/main/java/com/test/usingDB/repository/MemberRepository.java
@@ -4,8 +4,11 @@ import com.test.usingDB.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // 첫번째 인자 : 어떤 Entity인지, 두번째 인자 : pk 어떤 타입인지
-public interface MemberRepository extends JpaRepository<MemberEntity, Long>
-{
+import java.util.Optional;
 
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long>//어떤 Entity인지, pk 어떤 타입인지
+{
+    // 이메일로 회원 정보 조회( select * from member_table where member_email=?)
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
 }
